69ea3ce32e0219e3eb10766ef06c6eb02e301781
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 09:42:14 +0430

    code for write message and send to whatsapp.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pyautogui
 from time import sleep
 from random import choice
+lst = ["hello!", "hi", "i am robot", "Rahimi"]
+for i in range(4):
+    sleep(1)
+    pyautogui.click(410, 986)
+    pyautogui.write(choice(lst), interval=0.1)
+    pyautogui.hotkey("shift", "enter")
+
+pyautogui.press("enter")
