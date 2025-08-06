2c16423f6f8be2c67699f884f2b0dee89fc69927
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 09:50:18 +0430

    read from file feature added.

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import pyautogui
 from time import sleep
 from random import choice, uniform
 
-messages = ["hello!", "hi", "I am robot", "Rahimi"]
+with open("messages.txt", "r", encoding="utf-8") as f:
+    messages = [line.strip() for line in f if line.strip()]
 
 x_click = 410
 y_click = 986
-
 num_messages = 4
 
 for i in range(num_messages):
-    sleep(uniform(0.5, 2))  
-    pyautogui.click(x_click, y_click) 
-    message = choice(messages) 
+    sleep(uniform(0.5, 2))
+    pyautogui.click(x_click, y_click)
+    message = choice(messages)
     pyautogui.write(message, interval=0.05)
-    pyautogui.press("enter")  
+    pyautogui.press("enter")
