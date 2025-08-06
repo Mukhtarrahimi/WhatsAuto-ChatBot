500c688ea84e8f6e3cb96652a78518ae5c7ec7f1
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 09:47:55 +0430

    new feature added.

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pyautogui
 from time import sleep
-from random import choice
-lst = ["hello!", "hi", "i am robot", "Rahimi"]
-for i in range(4):
-    sleep(1)
-    pyautogui.click(410, 986)
-    pyautogui.write(choice(lst), interval=0.1)
-    pyautogui.hotkey("shift", "enter")
+from random import choice, uniform
 
-pyautogui.press("enter")
+messages = ["hello!", "hi", "I am robot", "Rahimi"]
+
+x_click = 410
+y_click = 986
+
+num_messages = 4
+
+for i in range(num_messages):
+    sleep(uniform(0.5, 2))  
+    pyautogui.click(x_click, y_click) 
+    message = choice(messages) 
+    pyautogui.write(message, interval=0.05)
+    pyautogui.press("enter")  
